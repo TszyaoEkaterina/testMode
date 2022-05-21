9a2ccc49a64f921e232a78be53f8d7a0fd3a4a02
Ekaterina <3022805283@qq.com>
2022-05-21 11:29:24 +0800

    data class modified

--- src/test/java/ru/netology/RegistrationDto.java
+++ src/test/java/ru/netology/RegistrationDto.java
@@ -18,16 +18,4 @@ private String status;
     public String getPassword() {
         return password;
     }
-    
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getAccountStatus() {
-//        return accountStatus;
-//    }
-//
-//    public void setAccountStatus(String accountStatus) {
-//        this.accountStatus = accountStatus;
-//    }
 }
